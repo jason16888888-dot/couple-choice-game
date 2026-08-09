e4e00e3a800aca1ff01143a9c1f0372b3a03362c
jason16888888-dot <breatheabe@gmail.com>
2026-08-09 13:18:09 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7gw8EKzbWPYNj-cvje6oG2-474J2ZnCM",
+  authDomain: "my-test-choose-one.firebaseapp.com",
+  projectId: "my-test-choose-one",
+  storageBucket: "my-test-choose-one.firebasestorage.app",
+  messagingSenderId: "612547065937",
+  appId: "1:612547065937:web:a71f60d6d74a82874ae070"
+};
